3b0849821a6250e376e87ccadbfb294feea5350e
Glen Mailer <glenjamin@gmail.com>
2013-09-09 12:15:00 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("                ________________\n\
+               ||====\\__//====||\n\
+               |j___  `--'  ___|j\n\
+                   |        |\n\
+                   |        |   hjw\n\
+                   |________|\n\
+");
+// Via http://www.retrojunkie.com/asciiart/clothing/shirts.htm
